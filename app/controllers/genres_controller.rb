1c60e60474beb31016fd2d108e49c1703b7e1dc3
Emmanuella Albuquerque <emmanuellaf.albuquerque@gmail.com>
2024-03-25 17:50:53 -0300

    Adicionando a base de Cadastro, Edição e Listagem de Gêneros

---
+++ app/controllers/genres_controller.rb
@@ -0,0 +1,33 @@
+class GenresController < ApplicationController
+  def new
+    @genre = Genre.new
+  end
+  
+  def create
+    @genre = Genre.new(name: params[:genre][:name])
+
+    if @genre.save()
+      return redirect_to genres_path()
+    end
+
+    render :new
+  end
+
+  def index
+    @genres = Genre.all
+  end
+
+  def edit
+    @genre = Genre.find(params[:id])
+  end
+
+  def update
+    @genre = Genre.find(params[:id])
+
+    if @genre.update(name: params[:genre][:name])
+      return redirect_to genres_path
+    end
+
+    render :edit
+  end
+end
